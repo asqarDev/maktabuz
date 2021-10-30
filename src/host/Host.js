a8315b9d25a45585f442429e9706e5e357c7fd5f
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-30 11:00:01 +0500

    sdvds

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const url = "http://143.244.209.138";
-export const idMaktab = "4";
-export const user = "9";
+export const idMaktab = "6";
+export const user = "38";
 export const httpRequest = (config) => {
   return axios({
     ...config,
